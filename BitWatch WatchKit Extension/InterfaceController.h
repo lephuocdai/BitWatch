94953957278a81de9bbcaccb336e20b2f814a8fa
lephuocdai <lephuocdaijapan@gmail.com>
2015-01-08 12:01:06 +0900

    Init commit

---
+++ BitWatch WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  BitWatch WatchKit Extension
+//
+//  Created by Phuoc Dai Le on 1/7/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
